845f86588d09ece591a8f3a826e28d6e8293e7fd
ehdgns565 <ehdgns322@gmail.com>
2024-01-06 16:02:23 +0900

    class import test 1

--- frontend/src/union/UnionWorker.js
+++ frontend/src/union/UnionWorker.js
@@ -3,20 +3,31 @@
 //   console.log('union worker thread execute')
 //   this.postMessage(true)
 // }
-import UnionRaiderSetting2 from './UnionRaiderSetting2';
+import * as React from 'react'
 
+var UnionRaiderSetting2 = ''
+if ('function' === typeof importScripts) {
+  UnionRaiderSetting2 = self.importScripts('UnionRaiderSetting2.js') // eslint-disable-line no-restricted-globals
+}
+// if (navigator.serviceWorker) {
+//   navigator.serviceWorker.register('src/union/UnionWorker.js', { scope: 'src/union' })
+//     .then(function(reg) {
+//       console.log('[Companion]', 'Service worker registered!');
+//     });
+// }
 export default () => {
   self.addEventListener('message', e => { // eslint-disable-line no-restricted-globals
     if (!e) return;
     let cnt = e.data.cnt;
-
     console.log('Received message from main thread : ', e.data)
     console.log('Received union block : ', e.data.unionBlock)
     console.log('Received table : ', e.data.table)
     console.log('Received cnt : ', e.data.cnt)
+    console.log('UnionRaiderSetting2 : ',UnionRaiderSetting2 ) 
+    // console.log('Received unionRaider : ', JSON.parse(e.data.setting)
     console.log('union worker thread execute')
 
-
+    // console.log('setting = ',setting)
     while (true) {
       // postMessage(cnt++);
       cnt++
@@ -29,19 +40,23 @@ export default () => {
     }
     postMessage(cnt++);
 
-    const setting = new UnionRaiderSetting2(e.data.unionBlock, JSON.parse(JSON.stringify(e.data.table)))
-    setting.parseRaider()
-    console.log('parse blocks Size= ', setting.blocksSize)
-    console.log('parse blocks= ', setting.blocks)
-    console.log('parse blocks binary = ', setting.blocksBinary)
-    console.log('parse count = ', setting.blocksCount)
-    setting.classify()
-    console.log('result count = ', setting.resultBlocksCount)
-    console.log('result table= ', setting.resultTable)
-    console.log('result domiBlocks=', setting.resultDomiBlocks)
+    // const setting = new UnionRaiderSetting2(e.data.unionBlock, JSON.parse(JSON.stringify(e.data.table)))
+    // setting.parseRaider()
+    // console.log('parse blocks Size= ', setting.blocksSize)
+    // console.log('parse blocks= ', setting.blocks)
+    // console.log('parse blocks binary = ', setting.blocksBinary)
+    // console.log('parse count = ', setting.blocksCount)
+    // setting.classify()
+    // console.log('result count = ', setting.resultBlocksCount)
+    // console.log('result table= ', setting.resultTable)
+    // console.log('result domiBlocks=', setting.resultDomiBlocks)
+
 
 
     console.log('worker end')
 
   })
+
 }
+
+
