38e10b657724e4ae3f5988c515fe8852e227b792
ehdgns565 <ehdgns322@gmail.com>
2024-01-13 15:59:03 +0900

    [ frontend ] 1. 셀 선택 시 hover color 지정 2. 테이블 바깥에서 드래그 유지시 내부로 다시 돌아왔을때 드래그 떼도 셀 선택가능한 오류 수정.

--- frontend/src/union/Table.js
+++ frontend/src/union/Table.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 
 // 상위 컴포넌트의 props를 props key 별로 받으려면 {}를 작성해줘야함. 그렇지 않으면 모든 props 가 한번에 map형태로 오게된다.
-export function BasicTable({ style , setTable}) {
+export function BasicTable({ style, setTable }) {
 
     // This array defines [iniRow, iniCol, endRow, endCol]
     const [select, setSelect] = React.useState([])
@@ -71,6 +71,10 @@ export function BasicTable({ style , setTable}) {
         }
     }
 
+    const preventMaintainDrag = (e) => {
+        setDrag(false)
+    }
+
     // 드래그하는 도중 전체 테이블셀을 계속 스캔
     const getClassName = (row, col) => {
         for (let i = 0; i < select.length; i++) {
@@ -84,25 +88,29 @@ export function BasicTable({ style , setTable}) {
 
 
     return (
-        <table className='union-table'
-        // style={{ borderSpacing: '0px' }}
-        >
-            <tbody>
-                {style.map((row, i) => (
-                    <tr key={i}>
-                        {row.map((v, j) => (
-                            <td
-                                onMouseDown={(e) => handleMouseDown(e, i, j)}
-                                onMouseUp={(e) => handleMouseUp(e, i, j)}
-                                onMouseMove={(e) => handleMultipleSel(e, i, j)}
-                                className={`union-table-cell ${getClassName(i, j)}`}
-                                key={`${j}`} style={v} uniqkey={`${i}-${j}`}>
-                            </td>
-                        ))}
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <div
+            // style={{ backgroundColor: '#b8b8b8', padding: 4 }}
+            onMouseMove={(e) => preventMaintainDrag(e)}>
+            <table className='union-table' onMouseMove={(e) => e.stopPropagation()}
+            // style={{ borderSpacing: '0px' }}
+            >
+                <tbody>
+                    {style.map((row, i) => (
+                        <tr key={i}>
+                            {row.map((v, j) => (
+                                <td
+                                    onMouseDown={(e) => handleMouseDown(e, i, j)}
+                                    onMouseUp={(e) => handleMouseUp(e, i, j)}
+                                    onMouseMove={(e) => handleMultipleSel(e, i, j)}
+                                    className={`union-table-cell ${getClassName(i, j)}`}
+                                    key={`${j}`} style={v} uniqkey={`${i}-${j}`}>
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
 
     );
 }
